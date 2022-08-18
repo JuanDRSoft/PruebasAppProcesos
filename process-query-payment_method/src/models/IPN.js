3a4adab842e8e28b4154281205082c9105f6a80c
JuanDRSoft <95110527+JuanDRSoft@users.noreply.github.com>
2022-08-17 20:13:33 -0500

    Delete process-query-payment_method directory

--- process-query-payment_method/src/models/IPN.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-let ipnShema = new mongoose.Schema({ resource: String, topic: String });
-
-const IPN = mongoose.model('IPN', ipnShema);
-
-module.exports = IPN;
