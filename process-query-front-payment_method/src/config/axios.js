b0d83ea50c0c0d9ea09a19949bc9be41968ab832
JuanDRSoft <95110527+JuanDRSoft@users.noreply.github.com>
2022-08-17 20:30:24 -0500

    .

--- process-query-front-payment_method/src/config/axios.js
+++ process-query-front-payment_method/src/config/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const clienteAxios = axios.create({
-  // baseURL: 'https://paymenth-method.herokuapp.com'
-  baseURL: 'http://localhost:7001'
+  baseURL: 'https://paymenth-method.herokuapp.com'
+  // baseURL: 'http://localhost:7001'
 });
 
 export default clienteAxios;
